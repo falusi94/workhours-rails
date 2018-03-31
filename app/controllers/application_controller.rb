75d39df5e48e73cb1fc2b9b81fc92a225216e244
David Falusi <falusi.david94@gmail.com>
2018-04-01 01:09:29 +0200

    Add require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
 
   def current_user
     @current_user ||= User.find(session[:user_id])
@@ -8,4 +9,10 @@ class ApplicationController < ActionController::Base
   def authorized?
     redirect_to :back unless current_user.admin?
   end
+
+  private
+    def require_login
+      redirect_to login_path unless session[:user_id] || ENV['NONAUTH']
+    end
+
 end
