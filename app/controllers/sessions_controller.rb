0503e006b0051f67aceaf2a0f80da99f23d70271
David Falusi <falusi.david94@gmail.com>
2018-04-22 19:42:10 +0200

    Fix logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login
-  before_action :require_logout
+  before_action :require_logout, only: [:new, :create]
 
   def new
   end
